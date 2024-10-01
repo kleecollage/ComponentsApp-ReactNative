1c3da41efa7c454684a31bea51d2d0907f36bd9a
Klee Collage <klee.collage@gmail.com>
2024-09-30 21:22:41 -0500

    Project structure

---
+++ src/ComponentsApp.tsx
@@ -0,0 +1,10 @@
+import './gesture-handler';
+import { Text, View } from 'react-native'
+
+export const ComponentsApp = () => {
+  return (
+    <View>
+      <Text>ComponentsApp</Text>
+    </View>
+  )
+}
---
+++ src/presentation/icons/Icons.tsx
@@ -0,0 +1,7 @@
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const myButton = (
+  <Icon.Button name="airplane-outline">
+    Login with Facebook
+  </Icon.Button>
+);
